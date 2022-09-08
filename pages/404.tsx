43f6882429578aa406440ab24886bd324ff500d4
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-09-08 19:36:32 +0530

    feat(404): updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,31 @@
-import { Container, Text } from "@nextui-org/react";
+import { Container, Text, Button, Spacer } from "@nextui-org/react";
 
 export default function NotFound() {
-    return (
-        <div>
-            <Container display="flex" alignItems="center" justify="center" css={{ height: "100vh" }}>
-                <Text h1 css={{ textGradient: "45deg, $red600 0%, $yellow600 50%, $pink600 75%" }}>Oops! The page you are looking for does not exist!</Text>
-            </Container>
-        </div>
-    )
+  return (
+    <Container
+      fluid
+      css={{
+        height: "100vh",
+        background:
+          "url(https://uploads-ssl.webflow.com/609124aa26f8d4863c88a6d9/6140b34198c4a2bf8e7e6224_1920%3A1080.png)",
+        backgroundSize: "cover",
+      }}
+    >
+      <Text
+        h1
+        size={96}
+        css={{ paddingTop: "$36", paddingLeft: "$36", color: "black" }}
+      >
+        404
+      </Text>
+      <Text
+        size={20}
+        css={{ paddingLeft: "$36", color: "black" }}
+      >
+        This page does not exist!
+      </Text>
+      <Spacer />
+      <Button css={{ marginLeft: "$36" }} auto ghost>Go Home</Button>
+    </Container>
+  );
 }
