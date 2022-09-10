3aea76dc00e9ff5743391e4873d41a292001325b
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-09-10 15:22:04 +0530

    fix: removed unused props

--- pages/create.tsx
+++ pages/create.tsx
@@ -3,34 +3,6 @@ import { Container, Input, Spacer, Text, Textarea } from "@nextui-org/react";
 import CreateFlow from "../components/CreateFlow";
 
 export default function Create() {
-  const nodes = [
-    {
-      id: "1",
-      type: "special",
-      data: { label: "Input Node" },
-      position: { x: 250, y: 25 },
-    },
-
-    {
-      id: "2",
-      type: "special",
-      // you can also pass a React component as a label
-      data: { label: <div>Default Node</div> },
-      position: { x: 100, y: 125 },
-    },
-    {
-      id: "3",
-      type: "special",
-      data: { label: "Output Node" },
-      position: { x: 250, y: 250 },
-    },
-  ];
-
-  const edges = [
-    { id: "e1-2", source: "1", target: "2" },
-    { id: "e2-3", source: "2", target: "3", animated: true },
-  ];
-
   return (
     <>
       <Navbar active={3} />
@@ -59,7 +31,7 @@ export default function Create() {
         />
         <Spacer y={2} />
         <div style={{ width: "50vw", height: "50vh" }}>
-          <CreateFlow nodes={nodes} edges={edges} />
+          <CreateFlow />
         </div>
       </Container>
     </>
